7687d09b66a296160cca920541e8ce2a2cc6fb91
jasonderulo44 <csokistank@gmail.com>
2024-07-30 13:14:37 +0300

    1 14 7 30 commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Company from './pages/Company';
 import Products from './pages/Products';
 import Contact from './pages/Contact';
 import Layout from './pages/Layout';
+import Technology from './pages/products/Technology';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/company" element={<Company />} />
           <Route path="/products" component={Products} />
+          <Route path="/products/technology" element={<Technology />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { IoLanguage } from 'react-icons/io5';
 import { useTranslation } from 'react-i18next';
 import { IoMdArrowDropdown } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
 import { HiOutlineBars3 } from 'react-icons/hi2';
 import '../Navbar.css';
 
@@ -13,29 +14,46 @@ const Layout = ({ children }) => {
 
     };
     const [menuOpen, setMenuOpen] = useState(false);
-    const [activeMenu, setActiveMenu] = useState(null);
+    const [dropdownOpen, setDropdownOpen] = useState(null);
+    const navbarRef = useRef(null);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
 
     const toggleDropdown = (index) => {
-        setActiveMenu(activeMenu === index ? null : index);
+        setDropdownOpen(dropdownOpen === index ? null : index);
     };
+
+    const handleClickOutside = (event) => {
+        if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+            setMenuOpen(false);
+            setDropdownOpen(null);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
     return (
         <div className="layout">
-            <nav className="navbar">
+            <nav className="navbar" ref={navbarRef}>
                 <div className="logo">
                     <Link to="/">
                         <img src="/WPowertech System logo black.png" alt="Logo" className='logo-black' />
                         <img src="/Wpowertech_logo_white.png" alt="Logo" className='logo-white' />
                     </Link>
                 </div>
-                <HiOutlineBars3 className="menu-icon" onClick={toggleMenu} />
+                <div className="menu-icon" onClick={toggleMenu}>
+                {menuOpen ? <IoMdClose /> : <HiOutlineBars3 />}
+            </div>
                 <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
                 <li className="dropdown" onClick={() => toggleDropdown(0)}>
                 <button type="button">{t('company')}<IoMdArrowDropdown/></button>
-                        <div className="dropdown-content">
+                <div className={`dropdown-content ${dropdownOpen === 0 ? 'show' : ''}`}>
                             <Link to="/company/news"><button type="button"><IoMdArrowDropdown className="dropdown-arrow" />{t('news')}</button></Link>
                             <Link to="/company/history"><button type="button">{t('history')}</button></Link>
                             <Link to="/company/certificates"><button type="button">{t('certificates')}</button></Link>
@@ -44,7 +62,7 @@ const Layout = ({ children }) => {
                     </li>
                     <li className="dropdown" onClick={() => toggleDropdown(1)}>
                     <button type="button">{t('products')}<IoMdArrowDropdown /></button>
-                        <div className={`dropdown-content ${activeMenu === 1 ? 'show' : ''}`}>
+                    <div className={`dropdown-content ${dropdownOpen === 1 ? 'show' : ''}`}>
                             <Link to="/products/ourproduct"><button><IoMdArrowDropdown className="dropdown-arrow" />{t('ourproduct')}</button></Link>
                             <Link to="/products/technology"><button type="button">{t('technology')}</button></Link>
                             <Link to="/products/references"><button type="button">{t('references')}</button></Link>
@@ -69,6 +87,9 @@ const Layout = ({ children }) => {
                     </button>
                     <button type="button" onClick={() => changeLanguage('de')}>
                         <img src="de.png" alt="German" />
+                    </button>
+                    <button type="button" onClick={() => changeLanguage('gr')}>
+                        <img src="gr.png" alt="Greek" />
                     </button>
                     <button type="button" onClick={() => changeLanguage('zh')}>
                         <img src="zh.png" alt="Chinese" />
