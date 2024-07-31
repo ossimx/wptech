f7f11b3ec7733dc36a8bf531649713bc1c853cf7
jasonderulo44 <csokistank@gmail.com>
2024-07-31 16:13:59 +0300

    7 31 4 13 commit

--- src/components/CarouselItems.js
+++ src/components/CarouselItems.js
@@ -1,38 +1,50 @@
 import React from 'react';
-import { FacebookEmbed } from 'react-social-media-embed';
-
+import images from '../images/Imageholder'
 
 const items = [
   <div className="facebook-post" key="1">
-    <FacebookEmbed
-      url="https://www.facebook.com/permalink.php?story_fbid=122126359934311964&amp;id=61559358922953"
-       className="facebook-embed"
-    />
+    <a
+      href="https://www.facebook.com/permalink.php?story_fbid=122126359934311964&id=61559358922953"
+    >
+      <img src={images.feed1} alt="Facebook Post 1" className="facebook-embed" />
+    </a>
   </div>,
   <div className="facebook-post" key="2">
-    <FacebookEmbed
-      url="https://www.facebook.com/permalink.php?story_fbid=122123720474311964&amp;id=61559358922953"
-       className="facebook-embed"
-    />
+    <a
+      href="https://www.facebook.com/permalink.php?story_fbid=122123720474311964&id=61559358922953"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={images.feed2} alt="Facebook Post 2" className="facebook-embed" />
+    </a>
   </div>,
   <div className="facebook-post" key="3">
-    <FacebookEmbed
-      url="https://www.facebook.com/permalink.php?story_fbid=122104183922311964&amp;id=61559358922953"
-       className="facebook-embed"
-    />
-    </div>,
-    <div className="facebook-post" key="4">
-      <FacebookEmbed
-        url="https://www.facebook.com/permalink.php?story_fbid=122108301158311964&amp;id=61559358922953"
-         className="facebook-embed"
-      />
-    </div>,
-    <div className="facebook-post" key="5">
-      <FacebookEmbed
-        url="https://www.facebook.com/permalink.php?story_fbid=122099210996311964&amp;id=61559358922953"
-         className="facebook-embed"
-      />
-    </div>,
+    <a
+      href="https://www.facebook.com/permalink.php?story_fbid=122104183922311964&amp;id=61559358922953"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={images.feed3} alt="Facebook Post 3" className="facebook-embed" />
+    </a>
+  </div>,
+  <div className="facebook-post" key="4">
+    <a
+      href="https://www.facebook.com/permalink.php?story_fbid=122099210996311964&id=61559358922953"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={images.feed4} alt="Facebook Post 4" className="facebook-embed" />
+    </a>
+  </div>,
+  <div className="facebook-post" key="5">
+    <a
+      href="https://fb.watch/tEHc3VA5v6/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={images.feed5} alt="Facebook Post 5" className="facebook-embed" />
+    </a>
+  </div>,
 ];
 
 export default items;
--- src/images/Imageholder.js
+++ src/images/Imageholder.js
@@ -13,7 +13,15 @@ import ro from './ro.png';
 import hu from './hu.png';
 import gr from './gr.png';
 import zh from './zh.png';
-
+import feed1 from './feed1.png';
+import feed2 from './feed2.png';
+import feed3 from './feed3.png';
+import feed4 from './feed4.png';
+import feed5 from './feed5.png';
+import tr from './tr.png';
+import certificate1 from './certificat1.png';
+import certificate2 from './certificat2.png'
+import certificate3 from './certificat3.png'
 
 const images = {
     Wpowertech1,
@@ -31,6 +39,15 @@ const images = {
     WPowertechSystemLogoBlack,
     WPowertechSystemLogoWhite,
     logo,
+    feed1,
+    feed2,
+    feed3,
+    feed4,
+    feed5,
+    tr,
+    certificate1,
+    certificate2,
+    certificate3,
   };
   
   export default images;
